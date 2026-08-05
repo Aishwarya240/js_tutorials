9bb35a5eb1bc6414517a89aa921fb260857853bf
Aishwarya <154302908+Aishwarya240@users.noreply.github.com>
2026-08-05 08:02:41 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -8,3 +8,25 @@ console.log(2
     + 
     6) //code redability should be high
 console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
