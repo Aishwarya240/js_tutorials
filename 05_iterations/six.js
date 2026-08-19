5decf5855163bb3351398b971759f255e77a9ee9
Aishwarya <154302908+Aishwarya240@users.noreply.github.com>
2026-08-19 05:56:13 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,34 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+const values = coding.forEach( (item) => {
+    //console.log(item);
+    return item
+})
+
+//console.log(values);
+// for each will not return values
+
+//const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//const newNums = myNums.filter( (num) => num > 4)
+//console.log(newNums);
+
+// whenever youre using scope u need to mention return {curly braces are used}
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//const newNums = myNums.filter( (num) => {
+//    return num > 4
+//} )
+
+//const newNums = []
+
+//myNums.forEach( (num) => {
+ //   if (num > 4) {
+   //     newNums.push(num)
+   // }
+//})
+
+//myNums.forEach ()
+//console.log(newNums);
